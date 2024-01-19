aba80e76081594a0c5dc6b4b5d74eef737c83986
abhishek-jain-1925 <jainabhishek1925@gmail.com>
2024-01-19 10:15:08 +0530

    Add New File of Assignments

---
+++ Day-3/Day3-Assignment-No-02.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+func findADay(no int) (result string, err error) {
+	day := map[int]string{
+		1: "Monday",
+		2: "Tuesday",
+		3: "Wednesday",
+		4: "Thursday",
+		5: "Friday",
+		6: "Saturday",
+		7: "Sunday",
+	}
+
+	result, ok := day[no]
+	if !ok {
+		err = fmt.Errorf("Not a valid day number!")
+	}
+
+	return result, err
+}
+
+func main() {
+	var index int
+	fmt.Println("Enter the number for which you want to Find an Equivalent Day:")
+	fmt.Scan(&index)
+
+	day, err := findADay(index)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Equivalent day:", day)
+	}
+}
