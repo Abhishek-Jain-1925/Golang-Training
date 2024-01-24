a33a6d1a8bba949ea5e8a6ea9e84a615b4a1cef3
Abhishek-Jain-1925 <jainabhishek1925@gmail.com>
2024-01-24 17:29:50 +0530

    Add New Files of Day-7

---
+++ Day-7/Day7-Assignment-No-02.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var wg sync.WaitGroup
+var inpStr string
+var m sync.Mutex
+
+func reverseString(inpStr string) {
+	var result string
+	for _, v := range inpStr {
+		result = string(v) + result
+	}
+	fmt.Println("Reversed String : ", result)
+	fmt.Println("Number of Goroutines : ", runtime.NumGoroutine())
+	wg.Done()
+}
+
+func main() {
+
+	fmt.Println("Enter String to Reverse : ")
+	m.Lock() //Used mutex concept for threadSafe value assignment!!
+	_, err := fmt.Scan(&inpStr)
+	m.Unlock()
+	if err != nil {
+		fmt.Println(fmt.Errorf("error !! Plz,Enter valid input !!"))
+		return
+	}
+
+	wg.Add(1)
+	go reverseString(inpStr)
+	wg.Wait()
+
+}
